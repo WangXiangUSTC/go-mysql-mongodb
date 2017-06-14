e97178c104366c4bc3a61da003c31f3aa8bd3c6f
WangXiangUSTC <347249478@qq.com>
2017-06-14 23:14:16 +0800

    modify import

--- river/river.go
+++ river/river.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/juju/errors"
 	"github.com/ngaut/log"
-	"github.com/wangxiang/mongodb"
+	"github.com/WangXiangUSTC/go-mysql-mongodb/mongodb"
 	"github.com/siddontang/go-mysql/canal"
 	"golang.org/x/net/context"
 )
